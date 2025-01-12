cdc9a6cd8f82ecd4fab5e9399a6c87ccc8855f11
LintangKastaraErlangga <acelintara@gmail.com>
2025-01-12 09:40:31 +0700

    bug fix

--- setlist/ProjectSetlist/src/main/resources/static/js/setlist_detail.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('textarea[name="content"]').addEventListener('input', function () {
-    this.style.height = 'auto'; // Reset height to calculate new size
-    this.style.height = this.scrollHeight + 'px'; // Adjust height to fit content
-});
